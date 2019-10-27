4a642ca2ebcb80e5d629ee17c802d23c49216bdf
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-28 01:06:56 +0800

    Added spacing to login form

--- src/login/components/TabLogin.tsx
+++ src/login/components/TabLogin.tsx
@@ -20,25 +20,29 @@ export default function TabLogin() {
 		<Paper>
 			<Box p={3}>
 				<form onSubmit={handleSubmit} autoComplete="off" noValidate>
-					<TextField
-						label="Username or Email"
-						value={values.name}
-						onChange={handleChange("name")}
-						margin="normal"
-						variant="outlined"
-						fullWidth
-					/>
-					<TextField
-						label="Password"
-						type="password"
-						value={values.password}
-						onChange={handleChange("password")}
-						margin="normal"
-						variant="outlined"
-						fullWidth
-					/>
-
+					<Box mt={1}>
+						<TextField
+							label="Username or Email"
+							value={values.name}
+							onChange={handleChange("name")}
+							margin="normal"
+							variant="outlined"
+							fullWidth
+						/>
+					</Box>
 					<Box mt={2}>
+						<TextField
+							label="Password"
+							type="password"
+							value={values.password}
+							onChange={handleChange("password")}
+							margin="normal"
+							variant="outlined"
+							fullWidth
+						/>
+					</Box>
+
+					<Box mt={3} mb={1}>
 						<Grid container spacing={3}>
 							<Grid item xs={2}></Grid>
 							<Grid item xs={2}></Grid>
