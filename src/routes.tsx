86f6cf1086ddf8211612dbd993fbddfccfa34f25
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-27 22:29:51 +0800

    Fixed spinner not centered

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,10 @@
 import { default as React, Suspense, lazy } from "react";
 import { Router, Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import { Grid } from "@material-ui/core";
 
 import App from "./";
+import Navbar from "./core/navbar";
 
 const routes = [{
 	path: "/home",
@@ -12,10 +14,27 @@ const routes = [{
 	component: "./login",
 }];
 
+function renderLoader() {
+	return(
+		<>
+			<Navbar />
+			<Grid
+				container
+				spacing={0}
+				direction="column"
+				justify="center"
+				alignItems="center"
+				style={{ minHeight: '100vh' }}>
+				<div className="spinner round"></div>
+			</Grid>
+		</>
+	);
+}
+
 export function Routes() {
 	return(
 		<Router history={createBrowserHistory()}>
-			<Suspense fallback={<div className="spinner round"></div>}>
+			<Suspense fallback={renderLoader()}>
 				<App routes={routes} />
 			</Suspense>
 		</Router>
