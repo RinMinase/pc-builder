3235c8578c507eb8af39713870c8193d13f6e69b
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-27 20:19:27 +0800

    Added login and registration tabs

--- src/login/components/ParallaxTop.tsx
+++ src/login/components/ParallaxTop.tsx
@@ -1,12 +1,29 @@
-import * as React from "react";
+import { default as React, useState } from "react";
 
-import { Box, Grid, Typography } from "@material-ui/core";
+import { AppBar, Box, Grid, Tab, Tabs, Typography } from "@material-ui/core";
+
+function TabPanel(props: any) {
+	const { children, value, index } = props;
+
+	return (
+		<Typography component="div" hidden={value !== index}>
+			<Box p={3}>{children}</Box>
+		</Typography>
+	);
+}
 
 export default function ParallaxTop() {
+	const [value, setValue] = useState(0);
+
+	const handleTabChange = (_event: any, newValue: number) => {
+		setValue(newValue);
+	};
+
 	return(
 		<>
 			<Grid container spacing={3}>
-				<Grid item xs={12} sm={6}>
+
+				<Grid item xs={12} sm={7}>
 					<Box mt={24} mb={2}>
 						<Typography align="center" variant="h4">Build your ideal setup now!</Typography>
 					</Box>
@@ -16,9 +33,24 @@ export default function ParallaxTop() {
 						<Typography align="center"> Suspendisse vitae diam interdum, vehicula tellus eu, posuere massa.</Typography>
 					</Box>
 				</Grid>
-				<Grid item xs={12} sm={6}>
-					<Typography align="center">Login / Register</Typography>
+
+				<Grid item xs={12} sm={5}>
+					<div>
+						<AppBar position="static">
+							<Tabs onChange={handleTabChange} value={value} centered>
+								<Tab label="Login" />
+								<Tab label="Register" />
+							</Tabs>
+						</AppBar>
+						<TabPanel value={value} index={0}>
+							Login
+						</TabPanel>
+						<TabPanel value={value} index={1}>
+							Register
+						</TabPanel>
+					</div>
 				</Grid>
+
 			</Grid>
 		</>
 	);
