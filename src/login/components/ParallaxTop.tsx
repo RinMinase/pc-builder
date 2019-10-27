abcab4a700a5a72112557a32d3a74bfe680862d1
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-27 17:47:26 +0800

    Added template contents to introduction box

--- src/login/components/ParallaxTop.tsx
+++ src/login/components/ParallaxTop.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 
-import { Grid, Typography } from "@material-ui/core";
+import { Box, Grid, Typography } from "@material-ui/core";
 
 export default function ParallaxTop() {
 	return(
 		<>
 			<Grid container spacing={3}>
 				<Grid item xs={12} sm={6}>
-					<Typography align="center">Introduction</Typography>
+					<Box mt={4} mb={2}>
+						<Typography align="center" variant="h4">Build your ideal setup now!</Typography>
+					</Box>
+					<Box mt={4}>
+						<Typography align="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod feugiat malesuada.</Typography>
+						<Typography align="center">Morbi blandit nisl eu faucibus eleifend. Fusce dapibus facilisis nisl vitae scelerisque.</Typography>
+						<Typography align="center"> Suspendisse vitae diam interdum, vehicula tellus eu, posuere massa.</Typography>
+					</Box>
 				</Grid>
 				<Grid item xs={12} sm={6}>
 					<Typography align="center">Login / Register</Typography>
