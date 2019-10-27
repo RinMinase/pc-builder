aa4ca6d8476359f52aac62cdc008adcf14db4a73
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-27 19:27:20 +0800

    Added content to footer component

--- src/login/components/ParallaxFooter.tsx
+++ src/login/components/ParallaxFooter.tsx
@@ -1,13 +1,24 @@
 import * as React from "react";
 
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Link, Typography } from "@material-ui/core";
+import { Favorite, GitHub } from "@material-ui/icons";
 
 export default function ParallaxFooter() {
 	return(
 		<>
 			<Grid container spacing={3}>
 				<Grid item xs={12}>
-					<Typography align="center">Footer</Typography>
+					<Typography align="center">
+						<Favorite color="error" />
+						<span>&nbsp;Open Source</span>
+					</Typography>
+					<Typography align="center">
+						<GitHub />
+						<span>&nbsp;&nbsp;</span>
+						<Link href="https://github.com/RinMinase/pc-builder"
+							target="_blank"
+							rel="noreferrer">View on GitHub</Link>
+					</Typography>
 				</Grid>
 			</Grid>
 		</>
