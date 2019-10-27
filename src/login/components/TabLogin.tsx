c216b48317f48a73bb500edce9685d07fbbbae56
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-28 00:43:22 +0800

    Added login form

--- src/login/components/TabLogin.tsx
+++ src/login/components/TabLogin.tsx
@@ -1,9 +1,56 @@
-import * as React from "react";
+import { default as React, useState, ChangeEvent } from "react";
 
-import { Box } from "@material-ui/core";
+import { Box, Button, Grid, Paper, TextField } from "@material-ui/core";
 
 export default function TabLogin() {
+	const [values, setValues] = useState({
+		name: "",
+		password: "",
+	});
+
+	const handleChange = (field: string) => (event: ChangeEvent<HTMLInputElement>) => {
+		setValues({ ...values, [field]: event.target.value });
+	};
+
+	const handleSubmit = (event: any) => {
+		event.preventDefault();
+	};
+
 	return(
-		<Box p={3}>Login</Box>
+		<Paper>
+			<Box p={3}>
+				<form onSubmit={handleSubmit} autoComplete="off" noValidate>
+					<TextField
+						label="Username or Email"
+						value={values.name}
+						onChange={handleChange("name")}
+						margin="normal"
+						variant="outlined"
+						fullWidth
+					/>
+					<TextField
+						label="Password"
+						type="password"
+						value={values.password}
+						onChange={handleChange("password")}
+						margin="normal"
+						variant="outlined"
+						fullWidth
+					/>
+
+					<Box mt={2}>
+						<Grid container spacing={3}>
+							<Grid item xs={2}></Grid>
+							<Grid item xs={2}></Grid>
+							<Grid item xs={2}></Grid>
+							<Grid item xs={6}>
+								<Button type="submit" variant="contained" color="primary" fullWidth>Login</Button>
+							</Grid>
+						</Grid>
+					</Box>
+
+				</form>
+			</Box>
+		</Paper>
 	);
 }
