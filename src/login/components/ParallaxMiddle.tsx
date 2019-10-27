6be13bdbd35dad811bf724d7372ef11d78d725ef
Rin Minase <rin.black.raison.detre@gmail.com>
2019-10-27 18:14:15 +0800

    Added content to middle parallax

--- src/login/components/ParallaxMiddle.tsx
+++ src/login/components/ParallaxMiddle.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 
-import { Grid, Typography } from "@material-ui/core";
+import { Box, Grid, Typography } from "@material-ui/core";
 
 export default function ParallaxMiddle() {
 	return(
 		<>
 			<Grid container spacing={3}>
 				<Grid item xs={12}>
-					<Typography align="center">Middle</Typography>
+					<Box mt={16} mb={2}>
+						<Typography align="center" variant="h4">Estimate your budgets</Typography>
+					</Box>
+					<Box mt={4} mb={24}>
+						<Typography align="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod feugiat malesuada.</Typography>
+						<Typography align="center">Morbi blandit nisl eu faucibus eleifend. Fusce dapibus facilisis nisl vitae scelerisque.</Typography>
+						<Typography align="center"> Suspendisse vitae diam interdum, vehicula tellus eu, posuere massa.</Typography>
+					</Box>
 				</Grid>
 			</Grid>
 		</>
