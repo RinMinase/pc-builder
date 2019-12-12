dd33b9ecd9c7561e4d7f1fb3cda5418b35a67518
Rin Minase <rin.black.raison.detre@gmail.com>
2019-12-12 23:28:34 +0800

    Updated sub-dependencies

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+import firebase from "firebase";
+
+const config = {
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	databaseURL: process.env.DATABASE_URL,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID
+}
+
+firebase.initializeApp(config);
+
+export default firebase;
